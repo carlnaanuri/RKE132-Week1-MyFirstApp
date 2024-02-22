e5835c049c38a9e790b6bfaa15a56b9ce7e3d313
Carl Naanuri <naanuri@hotmail.com>
2024-02-22 16:51:05 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// This is a comment to my code
+
+//Rakendus palub kasutajal sisestada oma nimi
+//Pärast nime sisestamist tervitatakse kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); // väljund
+//string - sõne
+string userName = Console.ReadLine(); //sisend
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //väljund
+
+// string interpolation
+Console.WriteLine($"Hello, {userName} !"); //väljund
